331ed2add842f5608e975fece2b714f543684095
thekatt0 <contact@katelynkuhn.com>
2020-01-18 08:04:02 -0600

    Making months, days and years lists

--- Chapter 7/datedetect.py
+++ Chapter 7/datedetect.py
@@ -6,16 +6,21 @@ import re
 
 text = '''Speaking of dates did you know 02/14/2020 is Valentine's Day? I really didn't know. The more you learn the the more you know. When I was born on that dark day 9/1/1989 I don't know if it was dark. The moon was new. The weather was normal california weather. And then in 10/01/2019 it was Halloween!'''
 
+#empty lists
 matches = []
+months = []
+days = []
+years = []
+
+#compiler
 monthRegex = re.compile(r'(\d{1,2})/(\d{1,2})/(\d{2,4})')
 
+#formating and appending lists
 for groups in monthRegex.findall(text):
+    months.append(groups[0])
     dates = '-'.join([groups[0],groups[1],groups[2]])
     matches.append(dates)
 
-month = groups[0]
-day = groups[1]
-year = groups[2]
 
-
-print(matches)
+#print
+print(months)
