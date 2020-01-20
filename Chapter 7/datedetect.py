0fc11f8b1bcaa89708fa3f7d7ac6e8488019889a
thekatt0 <contact@katelynkuhn.com>
2020-01-20 05:46:19 -0600

    Pyper clip added

--- Chapter 7/datedetect.py
+++ Chapter 7/datedetect.py
@@ -3,8 +3,9 @@
 
 
 import re
+import pyperclip
 
-text = '''Speaking of dates did you know 02/14/2020 is Valentine's Day? I really didn't know. The more you learn the the more you know. When I was born on that dark day 9/1/1989 I don't know if it was dark. The moon was new. The weather was normal california weather. And then in 10/01/2019 it was Halloween!'''
+text = '''Speaking of dates did you know 32/14/2020 is Valentine's Day? I really didn't know. The more you learn the the more you know. When I was born on that dark day 1/9/1989 I don't know if it was dark. The moon was new. The weather was normal california weather. And then in 10/01/2019 it was Halloween!'''
 
 #empty lists
 matches = []
@@ -13,14 +14,23 @@ days = []
 years = []
 
 #compiler
-monthRegex = re.compile(r'(\d{1,2})/(\d{1,2})/(\d{2,4})')
+dateRegex = re.compile(r'(\d{1,2})/(\d{1,2})/(\d{2,4})')
+
+monthRegex = re.compile(r'0[1-9]|1[0-9]|2[0-9]|3[0-1]')
 
 #formating and appending lists
-for groups in monthRegex.findall(text):
-    months.append(groups[0])
+
+
+for groups in dateRegex.findall(text):
+
     dates = '-'.join([groups[0],groups[1],groups[2]])
     matches.append(dates)
 
 
-#print
-print(months)
+if len(matches) > 0:
+    pyperclip.copy('n'.join(matches))
+    print('Copied to clipboard: ')
+    print('\n'.join(matches))
+else:
+    print('No dates found.')
+
